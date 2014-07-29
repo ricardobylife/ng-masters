be8454ba78956b3da7b7df94d6418236db24bc70
Ricardo Ribeiro <ricardo@bylife.com.br>
2014-07-29 12:12:56 -0300

    funcionando

--- js/app.js
+++ js/app.js
@@ -1,32 +1,44 @@
 $(document).ready(function(){
 	function vaibuscarasissues(){
+		function GithubRepo(username, reponame){
+			GithubRepo.prototype.repo = 'https://api.github.com/repos/'+username+'/'+reponame+'/issues';
+			
+			GithubRepo.prototype.busca_issues_e_popula_tabela = function(table){
+				$.ajax({
+					url: this.repo,
+					dataType: 'json',
+					success: function(issues){
+						if( typeof issues == 'string' ){
+							issues = JSON.parse(issues);
+						}
+						console.log(issues);
+						$.each(issues, function(){
+							var html = '<tr>';
+							html 	+= '<td>'+this.number+'</td><td>'+this.title+'</td>';
+							html 	+= '</tr>'
+							$(table).append(html)
+						});
+					},
+					error: function(zica){
+						console.log(zica);
+						console.log('deu zica aqui mano: ' + this.repo);
+					},
+					complete: function(){
+						var tr =$(table).find('td').parent();
+						tr.removeClass('odd');
+						tr.filter(':odd').addClass('odd');
+					}
+				});
+			}
+		}
 		var username = $('#user').val();
 		var reponame = $('#reponame').val();
 
-		function GithubRepo(username, reponame){
-			var url = "https://api.github.com/repos/"+username+"/"+reponame+"/issues";
-			$.get(url).success(function(data){
-				console.log(data);
-				window.issues = JSON.parse(data);
-			});			
-			return {
-				busca_issues_e_popula_tabela : function(table){
-					$(table).html(issues);
-				},
-			}
-		}
-
-		//mais um pouco de codigo aqui....
-
-		var le_repo = GithubRepo(username, reponame);
+		var le_repo = new GithubRepo(username, reponame);
 		le_repo.busca_issues_e_popula_tabela("#issuestable");
 	}
-	$('.box button').click(function(){
 
-		if($('#user').val() == '' || ($('#reponame').val() == '')){
-			alert('Preencha os campos!');
-		} else {
-			vaibuscarasissues();
-		}
-	})
+	$('#btn-busca').click(function(){
+		vaibuscarasissues();
+	});
 });
